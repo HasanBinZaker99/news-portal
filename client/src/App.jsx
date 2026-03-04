802c0b111bd193d458d54520270332a28710b75f
Md Hasan Zaker <mdhasanzaker@02:ea:02:59:11:ec.home>
2026-03-04 19:48:03 -0330

    Nested Route Manage part1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./dashboard/pages/Login";
 import MainLayout from "./dashboard/layout/MainLayout";
 import AdminIndex from "./dashboard/pages/AdminIndex";
+import ProtectDashboard from "./middleware/ProtectDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<MainLayout />}>
-          <Route path="" element={<Navigate to="/dashboard/admin" />}>
+        <Route path="/dashboard" element={<ProtectDashboard />}>
+          <Route path="" element={<MainLayout />}>
+            <Route path="" element={<Navigate to="/dashboard/admin" />} />
             <Route path="admin" element={<AdminIndex />} />/
           </Route>
         </Route>
